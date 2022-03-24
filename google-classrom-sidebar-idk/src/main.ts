d96f56738214024f53bb15e928f69e9b646dcf4b
Syncxv <aruldevarackal1@gmail.com>
2022-03-24 11:44:01 +0530

    sidebar open logic eh

--- google-classrom-sidebar-idk/src/main.ts
+++ google-classrom-sidebar-idk/src/main.ts
@@ -1,1 +1,8 @@
 import './styles/_main.scss'
+
+const menuButton = document.querySelector<HTMLButtonElement>('.menu-button')!
+const bruh = document.querySelector<HTMLButtonElement>('.bruh')!
+const sidebar = document.querySelector<HTMLDivElement>('.sidebar')!
+console.log(sidebar)
+menuButton.addEventListener('click', () => sidebar.classList.toggle('opened'))
+bruh.addEventListener('click', () => sidebar.classList.remove('opened'))
