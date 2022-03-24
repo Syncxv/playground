a837ebb1b89df24ccad20decf8f135f8e60eb84e
Syncxv <aruldevarackal1@gmail.com>
2022-03-24 15:50:34 +0530

    SLIDING ANIMATION for the nav thingy DONE :D

--- google-classrom-sidebar-idk/src/main.ts
+++ google-classrom-sidebar-idk/src/main.ts
@@ -3,6 +3,26 @@ import './styles/_main.scss'
 const menuButton = document.querySelector<HTMLButtonElement>('.menu-button')!
 const bruh = document.querySelector<HTMLButtonElement>('.bruh')!
 const sidebar = document.querySelector<HTMLDivElement>('.sidebar')!
-console.log(sidebar)
-menuButton.addEventListener('click', () => sidebar.classList.toggle('opened'))
+const items = document.querySelector<HTMLDivElement>('.items')!
+const arrItems = Array.from(items.children)
+arrItems[0].classList.add('selected')
+items.style.setProperty('--width', `${arrItems[0].getBoundingClientRect().width}px`)
+menuButton.addEventListener('click', e => sidebar.classList.toggle('opened'))
 bruh.addEventListener('click', () => sidebar.classList.remove('opened'))
+
+const heheClick = (e: Event) => {
+    arrItems.find(s => s.classList.contains('selected'))?.classList.remove('selected')
+    ;(e.target as HTMLDivElement).classList.add('selected')
+    const width = (e.target as HTMLDivElement).getBoundingClientRect().width
+    const index = arrItems.indexOf(e.target as HTMLDivElement)
+    items.style.setProperty('--width', `${width}px`)
+    if (!index) return items.style.setProperty('--offset', `0px`)
+    const offfsetWidth = arrItems
+        .slice(0, index)
+        .map(s => s.getBoundingClientRect().width)
+        .reduce((prev, curr) => prev + curr)
+    items.style.setProperty('--offset', `${offfsetWidth}px`)
+}
+
+arrItems.forEach(s => s.addEventListener('click', e => heheClick(e)))
+//OOOH I NEED TO GET EVERY ONE NOT JUST THE LAST ONE IM SO STUPID
