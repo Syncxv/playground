f285ea11ce909de127661a77215f1a4e1907aaec
Syncxv <aruldevarackal1@gmail.com>
2022-03-25 15:07:05 +0530

    LIVE REALOD AND TS DONE :D WOOOO

--- cursor-thing/gulpfile.js
+++ cursor-thing/gulpfile.js
@@ -1,56 +1,62 @@
-const gulp = require("gulp");
-const browserSync = require("browser-sync");
-const autoPrefixer = require("gulp-autoprefixer");
-const sass = require("gulp-sass")(require("sass"));
-const del = require("del");
+const gulp = require('gulp')
+const browserSync = require('browser-sync')
+const autoPrefixer = require('gulp-autoprefixer')
+const sass = require('gulp-sass')(require('sass'))
+const del = require('del')
+const webpack = require('webpack-stream')
+gulp.task('default', done => {
+    gulp.series('serve')(done)
+})
 
-gulp.task("default", (done) => {
-    gulp.series("serve")(done);
-});
+gulp.task('serve', done => {
+    gulp.series('clean', gulp.parallel('sass', 'ts', 'html'), 'browsersync', 'watch')(done)
+})
 
-gulp.task("serve", (done) => {
-    gulp.series("clean", gulp.parallel("sass" /*'js' */, "html"), "browsersync", "watch")(done);
-});
-
-gulp.task("sass", () => {
+gulp.task('sass', () => {
     return gulp
-        .src("./src/scss/main.scss")
+        .src('./src/scss/main.scss')
         .pipe(
             sass({
-                includePaths: "node_modules",
+                includePaths: 'node_modules'
             })
         )
         .pipe(autoPrefixer())
-        .pipe(gulp.dest("./dist/assets/css/"))
-        .pipe(browserSync.stream({ match: "**/*.css" }));
-});
+        .pipe(gulp.dest('./dist/assets/css/'))
+        .pipe(browserSync.stream({ match: '**/*.css' }))
+})
 
-gulp.task("html", () => {
-    return gulp.src("./src/*.html").pipe(gulp.dest("dist"));
-});
-
-gulp.task("browsersync", (done) => {
+gulp.task('html', () => {
+    return gulp.src('./src/*.html').pipe(gulp.dest('dist'))
+})
+gulp.task('ts', () => {
+    return gulp
+        .src(['./src/ts/**/*'])
+        .pipe(webpack(require('./webpack.config.js')))
+        .pipe(gulp.dest('./dist/assets/js'))
+})
+gulp.task('browsersync', done => {
     browserSync.init({
-        server: ["./dist", "./src/static"],
+        server: ['./dist', './src/static'],
         notify: false,
         ui: false,
         online: false,
         ghostMode: {
             clicks: false,
             forms: false,
-            scroll: false,
-        },
-    });
-    done();
-});
+            scroll: false
+        }
+    })
+    done()
+})
 
-gulp.task("watch", () => {
-    global.isWatching = true;
+gulp.task('watch', () => {
+    global.isWatching = true
 
-    gulp.watch("./src/scss/**/*.scss", gulp.series("sass"));
-    // gulp.watch("./src/js/**/*.*", gulp.series('js'));
-});
+    gulp.watch('./src/scss/**/*.scss', gulp.series('sass')).on('change', browserSync.reload)
+    gulp.watch('./src/ts/**/*.*', gulp.series('ts')).on('change', browserSync.reload)
+    gulp.watch('./src/*.html', gulp.series('html')).on('change', browserSync.reload)
+})
 
-gulp.task("clean", () => {
-    return del(["./tmp/**/*"], { dot: true });
-});
+gulp.task('clean', () => {
+    return del(['./tmp/**/*'], { dot: true })
+})
--- cursor-thing/webpack.config.js
+++ cursor-thing/webpack.config.js
@@ -1,3 +1,29 @@
-const webpack = require("webpack");
-
-module.exports = {};
+const webpack = require('webpack')
+const path = require('path')
+/** @type {import('webpack').Configuration} */
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: path.resolve(__dirname, './src/ts/index.ts'),
+    output: {
+        path: __dirname,
+        filename: 'bundle.js',
+        publicPath: '/assets/js/'
+    },
+    module: {
+        rules: [
+            { test: /\.ts?$/, loader: 'ts-loader' },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /(node_modules|bower_components)/,
+                options: {
+                    compact: true
+                }
+            }
+        ]
+    },
+    resolve: {
+        modules: ['./src/ts', 'node_modules']
+    }
+}
