c2a70b16b07260f84e73609a2b14af056c0046a0
Syncxv <aruldevarackal1@gmail.com>
2022-03-31 02:49:04 +0530

    COLORS

--- FIRST-nuxt/tailwind.config.js
+++ FIRST-nuxt/tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        'yt-500': '#4C4C4C',
+        'yt-700': '#212121',
+        'yt-800': '#181818',
+      },
       spacing: {
         0.1: '1px',
         0.2: '2px',
