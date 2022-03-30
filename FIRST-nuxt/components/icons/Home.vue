07156d4dd3a0aac4e86a386076127f30f8a3cb1c
Syncxv <aruldevarackal1@gmail.com>
2022-03-31 02:49:00 +0530

    moved to icons folder

---
+++ FIRST-nuxt/components/icons/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <svg
+    viewBox="0 0 24 24"
+    preserveAspectRatio="xMidYMid meet"
+    focusable="false"
+    class="w-6 h-6 fill-current"
+  >
+    <g class="style-scope yt-icon">
+      <path d="M4,10V21h6V15h4v6h6V10L12,3Z" class="style-scope yt-icon"></path>
+    </g>
+  </svg>
+</template>
