57766623c28edb3e15e4ee4280896f6fc7c31b1b
Syncxv <aruldevarackal1@gmail.com>
2022-03-31 00:05:28 +0530

    also removed src folder

---
+++ FIRST-nuxt/pages/youtube/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-blue-500 flex flex-col h-screen">
+    <nav
+      class="flex-none flex items-center justify-between bg-red-300 h-14 px-4"
+    >
+      <div class="flex gap-2">
+        <div class="flex flex-col gap-1 icon">
+          <div class="h-0.2 w-8 bg-black"></div>
+          <div class="h-0.2 w-8 bg-black"></div>
+          <div class="h-0.2 w-8 bg-black"></div>
+        </div>
+      </div>
+      <div>two</div>
+      <div>three</div>
+    </nav>
+    <main class="flex 1">main sheet</main>
+  </div>
+</template>
