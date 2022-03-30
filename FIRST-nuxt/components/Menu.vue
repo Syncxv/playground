d3fb8c51d9571a0ab5ab2e526c979ce51ad3066e
Syncxv <aruldevarackal1@gmail.com>
2022-03-31 00:51:12 +0530

    navbar 60% done

---
+++ FIRST-nuxt/components/Menu.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex flex-col gap-1 cursor-pointer icon">
+    <div class="h-0.1 w-5 bg-white"></div>
+    <div class="h-0.1 w-5 bg-white"></div>
+    <div class="h-0.1 w-5 bg-white"></div>
+  </div>
+</template>
--- FIRST-nuxt/pages/youtube/index.vue
+++ FIRST-nuxt/pages/youtube/index.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="bg-blue-500 flex flex-col h-screen">
     <nav
-      class="flex-none flex items-center justify-between bg-red-300 h-14 px-4"
+      class="flex-none flex items-center justify-between bg-gray-800 h-14 px-6 text-sm text-white"
     >
-      <div class="flex gap-2">
-        <div class="flex flex-col gap-1 icon">
-          <div class="h-0.2 w-8 bg-black"></div>
-          <div class="h-0.2 w-8 bg-black"></div>
-          <div class="h-0.2 w-8 bg-black"></div>
-        </div>
+      <div class="flex items-center gap-6">
+        <Menu />
+        <YoutubeLogo />
       </div>
-      <div>two</div>
+      <div class="flex">
+        <input
+          class="bg-gray-900 outline-none p-2 w-80"
+          placeholder="Search"
+          type="text"
+        />
+        <button class="border-none bg-gray-600 px-3">
+          <Search />
+        </button>
+      </div>
       <div>three</div>
     </nav>
     <main class="flex 1">main sheet</main>
