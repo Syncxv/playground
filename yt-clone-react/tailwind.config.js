06acf923e95135b1cb8f146ac9032567fef5509f
Syncxv <aruldevarackal1@gmail.com>
2022-03-31 06:37:27 +0530

    new color eh

--- yt-clone-react/tailwind.config.js
+++ yt-clone-react/tailwind.config.js
@@ -1,21 +1,19 @@
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'yt-500': '#4C4C4C',
-        'yt-700': '#212121',
-        'yt-800': '#181818',
-        'yt-900': '#121212',
-      },
-      spacing: {
-        0.1: '1px',
-        0.2: '2px',
-      },
+    content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                'yt-400': '#696972',
+                'yt-500': '#4C4C4C',
+                'yt-700': '#212121',
+                'yt-800': '#181818',
+                'yt-900': '#121212'
+            },
+            spacing: {
+                0.1: '1px',
+                0.2: '2px'
+            }
+        }
     },
-  },
-  plugins: [],
+    plugins: []
 }
