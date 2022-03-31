3b5d6fd254bab4b044cf5b63ac5246824564fd56
Syncxv <aruldevarackal1@gmail.com>
2022-03-31 08:34:08 +0530

    OK verfied now wokrs :D

--- yt-clone-react/src/components/VideoCard/index.tsx
+++ yt-clone-react/src/components/VideoCard/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { Check } from 'phosphor-react'
 import { timeSince } from '../../util/timeAgo'
 
 interface Props {
@@ -25,8 +26,19 @@ const VideoCard: React.FC<Props> = ({ video, user }) => {
                     <div className="text-base">
                         <p>{video.title}</p>
                         <div className="text-sm text-yt-400">
-                            <div className="username">
+                            <div className="username flex items-center gap-2">
                                 <span>{user.username}</span>
+                                {user.verified && (
+                                    <div className="relative h-3 w-3  rounded-full bg-slate-400 p-2">
+                                        <Check
+                                            className="absolute top-1/2 left-1/2"
+                                            style={{ transform: 'translate(-50%, -50%)' }}
+                                            weight="bold"
+                                            color="black"
+                                            size={12}
+                                        />
+                                    </div>
+                                )}
                             </div>
                             <span>
                                 {video.views} views • {timeSince(video.uploadDate)} ago
--- yt-clone-react/src/pages/index.tsx
+++ yt-clone-react/src/pages/index.tsx
@@ -5,16 +5,16 @@ import VideoCard from '../components/VideoCard'
 const Home: NextPage = () => {
     return (
         <>
-            <div className="all-shit grid grid-cols-auto-fit content-center justify-items-center gap-10 p-8">
-                {Array.from(Array(10)).map((_, i) => (
+            <div className="all-shit grid grid-cols-auto-fit place-content-center justify-items-center gap-10 p-8">
+                {Array.from(Array(20)).map((_, i) => (
                     <VideoCard
                         video={{
                             title: 'The Heap: what does malloc() do? - bin 0x14',
                             thumbnail: '/liveoverflow.jpg',
-                            uploadDate: new Date(),
+                            uploadDate: new Date(Date.now() - 24 * 60 * 60 * 1000 * 500),
                             views: '100k'
                         }}
-                        user={{ avatar: '/pfp.jpg', username: 'bruv', verified: true }}
+                        user={{ avatar: '/pfp.jpg', username: 'bruv', verified: Math.random() > 0.5 }}
                     />
                 ))}
             </div>
