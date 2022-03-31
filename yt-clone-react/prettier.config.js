81a091c34e6bf9fa441ef33b57779795f5ceb7a5
Syncxv <aruldevarackal1@gmail.com>
2022-03-31 06:13:36 +0530

    pretteir and phosphor icons eh

--- yt-clone-react/prettier.config.js
+++ yt-clone-react/prettier.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  arrowParens: 'avoid',
   singleQuote: true,
+  bracketSpacing: true,
+  endOfLine: 'lf',
   semi: false,
+  tabWidth: 4,
+  trailingComma: 'none',
+  printWidth: 110,
 }
