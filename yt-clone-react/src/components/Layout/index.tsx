9210f2abde765e7420bd5f94b9d6b8cb8bd78da6
Syncxv <aruldevarackal1@gmail.com>
2022-03-31 05:45:00 +0530

    moved nav to layout

--- yt-clone-react/src/components/Layout/index.tsx
+++ yt-clone-react/src/components/Layout/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import NavBar from '../NavBar'
 
 interface Props {}
 
-const Layout: React.FC<Props> = ({children}) => {
+const Layout: React.FC<Props> = ({ children }) => {
   return (
-    <div className="bg-blue-500 flex flex-col h-screen">
-        {children}
+    <div className="flex h-screen flex-col bg-blue-500">
+      <NavBar />
+      <div className="flex flex-1 overflow-y-hidden">
+        <main className="flex-1 bg-yt-800 text-sm text-gray-100">
+          {children}
+        </main>
+      </div>
     </div>
   )
 }
--- yt-clone-react/src/pages/index.tsx
+++ yt-clone-react/src/pages/index.tsx
@@ -1,15 +1,8 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import Layout from '../components/Layout'
-import NavBar from '../components/NavBar'
 
 const Home: NextPage = () => {
-  return (
-    <Layout>
-      <NavBar />
-    </Layout>
-  )
+  return <Layout></Layout>
 }
 
 export default Home
